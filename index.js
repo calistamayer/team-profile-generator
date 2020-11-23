423c3bb6c650e03c2900057f6211745df03b7e29
Calista Mayer <calista.a.mayer@gmail.com>
2020-11-22 16:22:31 -0800

    prompts user using inquirer

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 const inquirer = require('inquirer');
+
+inquirer.prompt([
+    {
+        type: 'input',
+        name: 'manager-name',
+        message: "What is your team manager's name?"
+    }
+]);
