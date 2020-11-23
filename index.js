11e71a5a3c913c1544732f84c4a58454b3c9f33b
Calista Mayer <calista.a.mayer@gmail.com>
2020-11-22 16:36:54 -0800

    team manager prompts

--- index.js
+++ index.js
@@ -3,7 +3,22 @@ const inquirer = require('inquirer');
 inquirer.prompt([
     {
         type: 'input',
-        name: 'manager-name',
-        message: "What is your team manager's name?"
+        name: 'name',
+        message: "Enter team manager's name"
+    },
+    {
+        type: 'input',
+        name: 'id',
+        message: "Enter team manager's employee ID"
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: "Enter team manager's email address"
+    },
+    {
+        type: 'input',
+        name: 'office',
+        message: "Enter team manager's office number"
     }
 ]);
